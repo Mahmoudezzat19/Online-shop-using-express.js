f675ae5a04fa4539d11638c17afa878e500b5f75
Mahmoud Ezzat <batsh@Mahmouds-MacBook-Air.local>
2021-07-13 18:08:31 +0200

    test.

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,6 +6,7 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     editing: false
   });
+  console.log("test");
 };
 
 exports.postAddProduct = (req, res, next) => {
