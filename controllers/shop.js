2dbe2ef4d280df1137067136dabac68129e1625d
Mahmoud Ezzat <batsh@Mahmouds-MacBook-Air.local>
2021-07-15 05:12:32 +0200

    add create and fetch a cart.

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,10 +46,20 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  
+  req.user.getCart()
+  .then(cart => {
+    return cart.getProducts()
+      .then(products => {
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: products
+        });
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
