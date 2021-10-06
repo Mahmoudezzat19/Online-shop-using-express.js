d2b63123bf0a01a39639601731b6ccd61a66a71a
mbatsh99 <35458488+mbatsh99@users.noreply.github.com>
2021-10-06 09:10:47 +0200

    add logout functionality.

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,8 +11,6 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = async (req, res, next) => {
     const user = await User.findByPk(1);
 
-    //const sessionUser = User.build({...user});
-    //req.session.sessionUser = sessionUser;
     req.session.user = user;
 
     console.log("user info stored in the session: ", req.session.user);
@@ -30,3 +28,10 @@ exports.getSignup = (req, res, next) => {
 }
 
 exports.postSignup = (req, res, next) => {}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+}
